56a9948887a77b8b0f2a13d93fc497d3c5c415a0
53by97 <srimanta_sahu@yahoo.com>
2016-10-17 20:01:11 +0530

    Initial commit..

---
+++ src/main/java/com/kvvssut/codemonk/sorting/_q2_PuchiAndLuggage.java
@@ -0,0 +1,66 @@
+package com.kvvssut.codemonk.sorting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _q2_PuchiAndLuggage {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		int inputs = Integer.parseInt(line);
+
+		int inputsArray[][] = new int[inputs][], sortedArray[][] = new int[inputs][];
+
+		for (int i = 0; i < inputs; i++) {
+			line = br.readLine();
+			int inputLen = Integer.parseInt(line);
+			inputsArray[i] = new int[inputLen];
+			sortedArray[i] = new int[inputLen];
+			for (int j = 0; j < inputLen; j++) {
+				inputsArray[i][j] = Integer.parseInt(br.readLine());
+				sortedArray[i][j] = inputsArray[i][j];
+			}
+		}
+
+		for (int i = 0; i < inputs; i++) {
+			int arrLen = inputsArray[i].length;
+			Arrays.sort(sortedArray[i]);
+
+			int index = 0, dist = 0, lastInput = 0;
+			for (int j = 0; j < arrLen; j++) {
+				int input = inputsArray[i][j];
+				if (input >= lastInput) {
+					for (; index < arrLen; index++) {
+						if (sortedArray[i][index] == input) {
+							sortedArray[i][index] = -1;
+							break;
+						}
+						if (sortedArray[i][index] != -1) {
+							dist++;
+						}
+					}
+				} else {
+					dist = 0;
+					for (index = 0; index < arrLen; index++) {
+						if (sortedArray[i][index] == input) {
+							sortedArray[i][index] = -1;
+							break;
+						}
+						if (sortedArray[i][index] != -1) {
+							dist++;
+						}
+					}
+				}
+
+				System.out.print(dist + " ");
+				lastInput = input;
+			}
+			System.out.println();
+		}
+	}
+
+}
