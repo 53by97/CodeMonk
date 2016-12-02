39dc3b239ad5bc48c0432faf5241465dd9c83556
53by97 <srimanta_sahu@yahoo.com>
2016-12-02 14:39:57 +0530

    package naming changed
    add ds, algo, basics before each type

---
+++ src/main/java/com/kvvssut/codemonk/ds/advanceddatastructures/TrieSearchEngineImpl.java
@@ -0,0 +1,112 @@
+package com.kvvssut.codemonk.ds.advanceddatastructures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TrieSearchEngineImpl {
+
+	public static final int ALPHABET_SIZE = 26;
+
+	public static void main(String[] args) throws IOException {
+		final String[] inputs = { "the", "a", "there", "answer", "any", "by", "bye", "their" };
+
+		TrieImplNode root = new TrieImplNode();
+		for (int i = 0; i < inputs.length; i++) {
+			insertKey(root, inputs[i]);
+		}
+
+		System.out.println("Enter prefix to search : ");
+
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(System.in));
+
+			StringBuilder stringBuilder = new StringBuilder();
+			char ch;
+			int length;
+			while ((ch = (char) reader.read()) != '\n') {
+				stringBuilder.append(ch);
+				if ((length = stringBuilder.length()) > 3) {
+					searchProbableWords(root, stringBuilder.toString(), 0, length, length + 7);
+				}
+			}
+
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
+
+	}
+
+	public static void insertKey(TrieImplNode root, final String key) {
+		int level, length = key.length(), index;
+
+		for (level = 0; level < length; level++) {
+			index = getIndexForChar(key.charAt(level));
+			if (root.getTrieArray()[index] == null) {
+				root.getTrieArray()[index] = new TrieImplNode();
+			}
+			root = root.getTrieArray()[index];
+		}
+		root.setLeaf(true);
+	}
+
+	public static boolean searchProbableWords(TrieImplNode node, final String key, int level, int length,
+			int maxLength) {
+
+		if (node != null) {
+			if (level == maxLength) {
+				
+
+			} else if (level > length && node.isLeaf()) {
+				
+
+			} else {
+				int index = getIndexForChar(key.charAt(level));
+				if (searchProbableWords(node.getTrieArray()[index], key, level + 1, length, maxLength)) {
+					
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public static int getIndexForChar(char ch) {
+		if (ch >= 95) {
+			return ch - 97;
+		} else {
+			return ch - 65;
+		}
+	}
+
+}
+
+class TrieImplNode {
+	private TrieImplNode trieArray[];
+	private boolean isLeaf;
+
+	public TrieImplNode() {
+		this.trieArray = new TrieImplNode[TrieSearchEngineImpl.ALPHABET_SIZE];
+		this.isLeaf = false;
+	}
+
+	public TrieImplNode[] getTrieArray() {
+		return trieArray;
+	}
+
+	public void setTrieImplNode(TrieImplNode[] trieArray) {
+		this.trieArray = trieArray;
+	}
+
+	public boolean isLeaf() {
+		return isLeaf;
+	}
+
+	public void setLeaf(boolean isLeaf) {
+		this.isLeaf = isLeaf;
+	}
+
+}
---
+++ src/main/java/com/kvvssut/codemonk/ds/arrayandstring/_q3_PrateekAndHisFriends.java
@@ -0,0 +1,55 @@
+package com.kvvssut.codemonk.ds.arrayandstring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _q3_PrateekAndHisFriends {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		int inputs = Integer.parseInt(line);
+
+		long inputsArray[][] = new long[inputs][], sumArray[] = new long[inputs];
+
+		for (int i = 0; i < inputs; i++) {
+			line = br.readLine();
+			String inputAndSum[] = line.split(" ");
+			int inputLen = Integer.parseInt(inputAndSum[0]);
+			sumArray[i] = Long.parseLong(inputAndSum[1]);
+
+			inputsArray[i] = new long[inputLen];
+			for (int j = 0; j < inputLen; j++) {
+				inputsArray[i][j] = Long.parseLong(br.readLine());
+			}
+		}
+
+		boolean matchFound;
+		long inputArr[], sum = 0l;
+		for (int i = 0; i < inputs; i++) {
+			matchFound = false;
+			inputArr = inputsArray[i];
+			for (int j = 0; j < inputArr.length; j++) {
+				sum = 0l;
+				for (int k = j; k < inputArr.length; k++) {
+					sum = sum + inputArr[k];
+					if (sum >= sumArray[i]) {
+						if (sum == sumArray[i]) {
+							matchFound = true;
+							j = inputArr.length;
+						}
+						break;
+					}
+				}
+			}
+			if (matchFound) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+	}
+
+}
